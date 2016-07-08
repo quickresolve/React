5b8c905915d5a439df9ea45d13b97d13427b0f16
QuickResolve <lauranmontoya@gmail.com>
2016-07-08 15:06:07 -0700

    create profile components

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,14 +1,47 @@
 // var app = document.getElementById('app');
 // app.innerHTML = 'Hello!'
 
+var USERDATA = {
+  name: 'Jane Doe',
+  username: 'JaneDoe',
+  image: 'http://irishdeaths.com/wp-content/uploads/2014/03/Jane-Doe-3-800x450.jpg'
+}
+
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+// var HelloWorld = React.createClass({
+//   render: function(){
+//     // console.log(this.props)
+//     return(
+//       <div> Hello {this.props.name}! </div>
+//       )
+//   }
+// });
+
+var ProfilePic = React.createClass({
   render: function(){
-    console.log(this.props)
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function(){
     return(
-      <div> Hello React JS Program! </div>
+      <div>
+        <a href={'https://www.github.com' + this.props.username} >
+          {this.props.username}
+        </a>
+      </div>
+      )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return(
+        <div>{this.props.name}</div>
       )
   }
 });
