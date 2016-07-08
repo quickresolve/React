9ac45737a0fa602bbb3b11696a9f6cf5c13faf81
QuickResolve <lauranmontoya@gmail.com>
2016-07-08 13:47:16 -0700

    create hello world class with react

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,2 +1,18 @@
-var app = document.getElementById('app');
-app.innerHTML = 'Hello!'
+// var app = document.getElementById('app');
+// app.innerHTML = 'Hello!'
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function(){
+    return(
+      <div> Hello World! </div>
+      )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+  )
