db856d45dfd0c1b8f591c36ef0cf85d53fdb4d79
QuickResolve <lauranmontoya@gmail.com>
2016-07-10 12:30:57 -0700

    add confirm battle component

--- github-battle/app/containers/ConfirmBattleContainer.js
+++ github-battle/app/containers/ConfirmBattleContainer.js
@@ -1,12 +1,21 @@
 var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function() {
+    return {
+      isLoading: true,
+      playersInfo: []
+    }
+  },
   render: function(){
     return(
-      <div></div>
+      <ConfirmBattle />
     )
   }
-
 });
 
 module.exports = ConfirmBattleContainer;
