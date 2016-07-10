35fb3d57d23694f9476f5455f9597f75427acbb5
QuickResolve <lauranmontoya@gmail.com>
2016-07-10 15:03:39 -0700

    update return of confirm battle component to display player info

--- github-battle/app/containers/ConfirmBattleContainer.js
+++ github-battle/app/containers/ConfirmBattleContainer.js
@@ -37,10 +37,19 @@ var ConfirmBattleContainer = React.createClass({
   componentWillUnmount: function() {
     console.log('componentWillUnmount');
   },
+  handleInitiateBattle: function(){
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        this.state.playersInfo
+      }
+    })
+  },
   render: function(){
     return(
       <ConfirmBattle
       isLoading={this.state.isLoading}
+      onInitiateBattle={this.handleInitiateBattle}
       playersInfo={this.state.playersInfo} />
     )
   }
