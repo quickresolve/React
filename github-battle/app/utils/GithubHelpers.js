5bf9388ed63161e4f29a95b7928f1dc755ef3057
QuickResolve <lauranmontoya@gmail.com>
2016-07-10 16:29:25 -0700

    add helpers for battle function

--- github-battle/app/utils/GithubHelpers.js
+++ github-battle/app/utils/GithubHelpers.js
@@ -9,6 +9,41 @@ function getUserInfo (username) {
   // returns a promise
 };
 
+
+function getRepos(username){
+  // fetch usernames repos
+   return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars(repos){
+  //calculate all the stars that the user has
+  return repos.data.reduce(function(prev, current){
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData(player) {
+  // Get repos
+  return getRepos(player.login)
+    .then(getTotalStars
+  // Pass to getTotalStars
+    .then(function(totalStars){
+      // Return an object with that data
+      return{
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players){
+  // return an array, after doing fancy algorithms to determine a winner
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo = function(players) {
     // This function will evaluate to an array of promises
@@ -25,6 +60,16 @@ var helpers = {
     // Fetch some data from Github
     // Using map lets you loop over each user in players array
     // axios.all evaluates the array of promises and returns when they are all done resolving
+  },
+  battle: function(players){
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(err){
+        console.warn('Error in getPlayersInfo: ', err )
+      })
   }
 };
 
