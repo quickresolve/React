c2357a905801896693744ffc76004685b3e75313
QuickResolve <lauranmontoya@gmail.com>
2016-07-11 14:41:42 -0700

    add history for router

--- weather-app/app/index.js
+++ weather-app/app/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Layout from "./components/Layout.js";
+import routes from "./config/routes.js";
 
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(routes, app);
 
 
