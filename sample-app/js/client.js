f6db243a325dbaffb23c7602e73cd15b88bc3637
QuickResolve <lauranmontoya@gmail.com>
2016-07-11 12:03:06 -0700

    add layout component

--- sample-app/js/client.js
+++ sample-app/js/client.js
@@ -1,22 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-  // const name = "Will";
-  // getVal(){
-  //   return "Will";
-  // }
-  constructor(){
-    super();
-    this.name = "Will";
-  }
-  render() {
-    return (
-      <h1>It's  {this.name}!</h1>
-    );
-  }
-}
+import Layout from "./components/Layout.js";
+
 
 const app = document.getElementById('app');
-
 ReactDOM.render(<Layout/>, app);
