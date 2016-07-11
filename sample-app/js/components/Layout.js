3fa9f5061fdf76483299d2c25e6ffb810a50c35d
QuickResolve <lauranmontoya@gmail.com>
2016-07-11 12:16:29 -0700

    rearrange imports

--- sample-app/js/components/Layout.js
+++ sample-app/js/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Header from "./Header";
 import Footer from "./Footer";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
   // const name = "Will";
